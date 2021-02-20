6cdef856f4ec12bbd4b8a7ddbc9e094f2c800fc7
sherifHelmy <helmysherif792@gmail.com>
2021-02-20 17:21:42 +0200

    headers section

---
+++ js/index.js
@@ -0,0 +1,80 @@
+$(".item1").hover(function(){
+    $("#product1").css("display" , "block");
+    $("#product2").css("display" , "none");
+    $("#product3").css("display" , "none");
+    $("#product4").css("display" , "none");
+    $("#product5").css("display" , "none");
+    $("#product6").css("display" , "none");
+    $("#product7").css("display" , "none");
+    $("#product8").css("display" , "none");
+})
+$(".item2").hover(function(){
+    $("#product1").css("display" , "none");
+    $("#product2").css("display" , "block");
+    $("#product3").css("display" , "none");
+    $("#product4").css("display" , "none");
+    $("#product5").css("display" , "none");
+    $("#product6").css("display" , "none");
+    $("#product7").css("display" , "none");
+    $("#product8").css("display" , "none");
+})
+$(".item3").hover(function(){
+    $("#product1").css("display" , "none");
+    $("#product2").css("display" , "none");
+    $("#product3").css("display" , "block");
+    $("#product4").css("display" , "none");
+    $("#product5").css("display" , "none");
+    $("#product6").css("display" , "none");
+    $("#product7").css("display" , "none");
+    $("#product8").css("display" , "none");
+})
+$(".item4").hover(function(){
+    $("#product1").css("display" , "none");
+    $("#product2").css("display" , "none");
+    $("#product3").css("display" , "none");
+    $("#product4").css("display" , "block");
+    $("#product5").css("display" , "none");
+    $("#product6").css("display" , "none");
+    $("#product7").css("display" , "none");
+    $("#product8").css("display" , "none");
+})
+$(".item5").hover(function(){
+    $("#product1").css("display" , "none");
+    $("#product2").css("display" , "none");
+    $("#product3").css("display" , "none");
+    $("#product4").css("display" , "none");
+    $("#product5").css("display" , "block");
+    $("#product6").css("display" , "none");
+    $("#product7").css("display" , "none");
+    $("#product8").css("display" , "none");
+})
+$(".item6").hover(function(){
+    $("#product1").css("display" , "none");
+    $("#product2").css("display" , "none");
+    $("#product3").css("display" , "none");
+    $("#product4").css("display" , "none");
+    $("#product5").css("display" , "none");
+    $("#product6").css("display" , "block");
+    $("#product7").css("display" , "none");
+    $("#product8").css("display" , "none");
+})
+$(".item7").hover(function(){
+    $("#product1").css("display" , "none");
+    $("#product2").css("display" , "none");
+    $("#product3").css("display" , "none");
+    $("#product4").css("display" , "none");
+    $("#product5").css("display" , "none");
+    $("#product6").css("display" , "none");
+    $("#product7").css("display" , "block");
+    $("#product8").css("display" , "none");
+})
+$(".item8").hover(function(){
+    $("#product1").css("display" , "none");
+    $("#product2").css("display" , "none");
+    $("#product3").css("display" , "none");
+    $("#product4").css("display" , "none");
+    $("#product5").css("display" , "none");
+    $("#product6").css("display" , "none");
+    $("#product7").css("display" , "none");
+    $("#product8").css("display" , "block");
+})
